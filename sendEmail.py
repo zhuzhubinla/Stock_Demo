2e85ec825b91b06e14e60f945af5f1f0d7a4eca3
zhu bin <zhu.zhubinlala@gmail.com>
2013-07-05 17:04:17 +0800

    Add connectio fucntion

--- sendEmail.py
+++ sendEmail.py
@@ -6,7 +6,25 @@ from email.MIMEMultipart import MIMEMultipart
 import smtplib
 import time
 
-def send_plaintext():
+def connection():
+    try:
+        smtp=smtplib.SMTP_SSL()
+        smtp.connect('smtp.gmail.com',465)
+        smtp.login('zhu.zhubinlala@gmail.com','160815zb@')
+        #smtp.sendmail(mail_from,mail_to,msg.as_string())
+        #smtp.quit()
+        print 'ok'
+    except exception,e:
+        print e
+
+    return smtp
+
+def logout(smtp):
+    smtp.quit()
+    
+
+
+def send_plaintext(smtp):
     mail_body='test message'
     mail_from='zhu.zhubinlala@gmail.com'
     mail_to=['allenbintestzhu@gmail.com']
@@ -14,17 +32,11 @@ def send_plaintext():
     msg['Subject']='test email'
     msg['To']=';'.join(mail_to)
     msg['date']=time.strftime('%Y-%m-%d',time.localtime(time.time()))
-    try:
-        smtp=smtplib.SMTP_SSL()
-        smtp.connect('smtp.gmail.com',465)
-        smtp.login('zhu.zhubinlala@gmail.com','160815zb@')
-        smtp.sendmail(mail_from,mail_to,msg.as_string())
-        smtp.quit()
-        print 'ok'
-    except exception,e:
-        print e
+    smtp.sendmail(mail_from,mail_to,msg.as_string())
 
-def send_emailwithattachment():
+
+
+def send_emailwithattachment(connection):
     mail_body='test message'
     mail_from='zhu.zhubinlala@gmail.com'
     mail_to=['allenbintestzhu@gmail.com']
@@ -50,7 +62,12 @@ def send_emailwithattachment():
         print e
 
 if __name__ == '__main__':
-    send_emailwithattachment()
+    smtp=connection()
+    for i in range(0,2,1):
+        send_plaintext(smtp)
+        #send_emailwithattachment()
+        time.sleep(6)
+    logout(smtp)
     
     
         
